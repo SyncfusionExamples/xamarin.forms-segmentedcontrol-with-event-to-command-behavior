6b1074bdad601089869df7a6bcef14618c467edd
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-08-05 17:48:23 +0530

    segment issue has been fixed

---
+++ SegmentSample/SegmentSample/ViewModel/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.XForms.Buttons;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SegmentSample
+{
+  public class ViewModel 
+    {
+        public Command SelectionChangedCommand
+        {
+            get;
+            set;
+        }
+        public ViewModel()
+        {
+            SelectionChangedCommand = new Command<Syncfusion.XForms.Buttons.SelectionChangedEventArgs>(SelectionChanged);
+            ItemCollection = new ObservableCollection<SfSegmentItem>
+                    {
+                        new SfSegmentItem() {  Text = "Once"},
+                        new SfSegmentItem() {  Text = "Daily"},
+                        new SfSegmentItem() {  Text = "Weekly"},
+                        new SfSegmentItem() {  Text = "Monthly"},
+                        new SfSegmentItem() {  Text = "Yearly"},
+                    };
+        }
+
+        private void SelectionChanged(Syncfusion.XForms.Buttons.SelectionChangedEventArgs obj)
+        {
+            Application.Current.MainPage.DisplayAlert("Notification", "The Selected Index: " + obj.Index, "Ok");
+        }
+
+        private ObservableCollection<SfSegmentItem> itemCollection = new ObservableCollection<SfSegmentItem>();
+      
+        public ObservableCollection<SfSegmentItem> ItemCollection
+        {
+            get { return itemCollection; }
+            set { itemCollection = value; }
+        }
+    }
+}
